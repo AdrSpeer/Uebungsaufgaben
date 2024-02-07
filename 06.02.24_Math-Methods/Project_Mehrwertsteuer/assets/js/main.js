d730c97c22ddeff6794d6920c031298024a77fd4
AdrSpeer <adn.speer@gmail.com>
2024-02-07 09:39:47 +0100

    added onChange

--- 06.02.24_Math-Methods/Project_Mehrwertsteuer/assets/js/main.js
+++ 06.02.24_Math-Methods/Project_Mehrwertsteuer/assets/js/main.js
@@ -1,3 +1,23 @@
+// ! Textänderung HTML
+const changeText = () => {
+  // Value u Output HTML
+  const label = document.querySelector(".betrag label");
+  const bruttoNetto = document.querySelector("#endbetrag");
+  const radioBerechnung = document.querySelector(
+    "input[name=satz]:checked"
+  ).value;
+
+  // Abfrage
+  if (radioBerechnung === "brutto") {
+    label.innerHTML = "Bruttobetrag (Preis inkl Mehrwertsteuer) in Euro";
+    bruttoNetto.innerHTML = "Nettobetrag";
+  } else if (radioBerechnung === "netto") {
+    label.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
+    bruttoNetto.innerHTML = "Bruttobetrag";
+  }
+};
+
+// ! Berechnug
 const rechner = () => {
   // Value HTML
   const radioBerechnung = document.querySelector(
@@ -11,49 +31,38 @@ const rechner = () => {
   // Output HTML
   const outputSteuer = document.querySelector(".steuerbetrag p");
   const outputNetto = document.querySelector(".nettobetrag p");
-  const label = document.querySelector(".betrag label");
-  const bruttoNetto = document.querySelector("#endbetrag");
 
   // Abfrage
 
   // - Brutto 19%
   if (radioBerechnung === "brutto" && radioSteuersatz === "neunzehn") {
-    // Textänderung HTML
-    label.innerHTML = "Bruttobetrag (Preis inkl Mehrwertsteuer) in Euro";
-    bruttoNetto.innerHTML = "Nettobetrag";
     // Berechnung
     let steuerRechnerBrutto = inputBetrag / 1.19;
     let bruttoRechner = Number(inputBetrag) - Number(steuerRechnerBrutto);
     // Output HTML
     outputSteuer.innerHTML = bruttoRechner.toFixed(2) + "€";
     outputNetto.innerHTML = steuerRechnerBrutto.toFixed(2) + "€";
+
     // - Netto 19%
   } else if (radioBerechnung === "netto" && radioSteuersatz === "neunzehn") {
-    // Textänderung HTML
-    label.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
-    bruttoNetto.innerHTML = "Bruttobetrag";
     // Berechnung
     let steuerRechnerNetto = inputBetrag * 0.19;
     let nettoRechner = Number(inputBetrag) + Number(steuerRechnerNetto);
     // Output HTML
     outputSteuer.innerHTML = steuerRechnerNetto.toFixed(2) + "€";
     outputNetto.innerHTML = nettoRechner.toFixed(2) + "€";
+
     // - Brutto 7%
   } else if (radioBerechnung === "brutto" && radioSteuersatz === "sieben") {
-    // Textänderung HTML
-    label.innerHTML = "Bruttobetrag (Preis inkl Mehrwertsteuer) in Euro";
-    bruttoNetto.innerHTML = "Nettobetrag";
     // Berechnung
     let steuerRechnerBrutto = inputBetrag / 1.07;
     let bruttoRechner = Number(inputBetrag) - Number(steuerRechnerBrutto);
     // Output HTML
     outputSteuer.innerHTML = bruttoRechner.toFixed(2) + "€";
     outputNetto.innerHTML = steuerRechnerBrutto.toFixed(2) + "€";
+
     // - Netto 7%
   } else if (radioBerechnung === "netto" && radioSteuersatz === "sieben") {
-    // Textänderung HTML
-    label.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
-    bruttoNetto.innerHTML = "Bruttobetrag";
     // Berechnung
     let steuerRechnerNetto = inputBetrag * 0.07;
     let nettoRechner = Number(inputBetrag) + Number(steuerRechnerNetto);
