837d8e8802d5c34ba6182b538acaf330492d98ba
AdrSpeer <adn.speer@gmail.com>
2024-02-19 15:57:56 +0100

    changed Month

--- 19.02.24_Date/Date-Level-2_1/assets/js/main.js
+++ 19.02.24_Date/Date-Level-2_1/assets/js/main.js
@@ -3,7 +3,7 @@ const output = document.querySelector(".output");
 const getDate = () => {
   const today = new Date();
   const day = today.getDate();
-  const month = today.getMonth();
+  const month = today.getMonth() + 1;
   const year = today.getFullYear();
 
   return (output.innerHTML = `${day}/${month}/${year}`);
